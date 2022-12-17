300b2bbab993a1addca1319ba9af51ab6b6a4557
nahyeon cho <whskgus12@nate.com>
2022-12-18 00:28:54 +0900

    feat : 게시글 수정 기능 구현
    기존 Board 객체를 id로 검색하여 수정하는 기능 구현 완료하였습니다.

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -33,7 +34,7 @@ public class BoardController {
     }
 
     /**
-     * 접근 방법 : http://localhost:8080/board/view?id=3 ( id로 상세페이지 접근 )
+     * 접근 방법 : http://localhost:8080/board/view?id=3 ( QueryString - id로 상세페이지 접근 )
      */
     @GetMapping("/board/view")
     public String boardView(Model model,Integer id) {
@@ -44,6 +45,28 @@ public class BoardController {
     @GetMapping("/board/delete")
     public String boardDelete(Integer id){
         boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    /**
+     * 접근 방법 : http://localhost:8080/board/3 ( PathVariable 사용 - 바로 id 값으로 접근 )
+     */
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model){
+
+        model.addAttribute("article",boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id,Board board){
+
+        Board oldBoard = boardService.boardView(id); // 기존 객체 불러오기
+        // 기존 객체에 덮어씌우기
+        oldBoard.setTitle(board.getTitle());
+        oldBoard.setContent(board.getContent());
+
+        boardService.write(oldBoard);
         return "redirect:/board/list";
     }
 }
