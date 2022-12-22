c11c0787356496eb1ea826c862cc2e92d242adbb
nahyeon cho <whskgus12@nate.com>
2022-12-22 15:14:22 +0900

    feat : 게시판 페이징 기능 추가
    게시판 페이지에 Pageable을 사용하여 페이징 기능 추가하였습니다.

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -3,12 +3,15 @@ package com.study.board.controller;
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
@@ -34,9 +37,23 @@ public class BoardController {
         return "message";
     }
 
+    /**
+     * pageable을 통해 localhost:8080/board/list?page=1&size=10
+     * page와 size를 넘겨줄 수 있다. ( @pageableDefault )
+     */
     @GetMapping("/board/list")
-    public String boardList(Model model) {
-        model.addAttribute("list", boardService.boardList());
+    public String boardList(Model model, Pageable pageable) {
+        Page<Board> list = boardService.boardList(pageable);
+
+        int nowPage = list.getPageable().getPageNumber();
+        int startPage = Math.max(nowPage - 2, 0);
+        int endPage = Math.min(nowPage + 3, list.getTotalPages()-1);
+
+        model.addAttribute("list", list); // 변수 넘겨주기
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
         return "boardlist";
     }
 
@@ -55,9 +72,6 @@ public class BoardController {
         return "redirect:/board/list";
     }
 
-    /**
-     * 접근 방법 : http://localhost:8080/board/3 ( PathVariable 사용 - 바로 id 값으로 접근 )
-     */
     @GetMapping("/board/modify/{id}")
     public String boardModify(@PathVariable("id") Integer id, Model model) {
 
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -3,6 +3,10 @@ package com.study.board.service;
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,22 +18,24 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     // 게시글 작성
-    public void write(Board board){
+    public void write(Board board) {
         boardRepository.save(board);
     }
 
     // 게시글 목록 처리
-    public List<Board> boardList(){
-        return boardRepository.findAll();
+    public Page<Board> boardList(Pageable pageable) {
+        int page = pageable.getPageNumber();
+        int pageLimit = 15;
+        return boardRepository.findAll(PageRequest.of(page, pageLimit, Sort.by(Sort.Direction.DESC, "id")));
     }
 
     // 특정 게시글 불러오기
-    public Board boardView(Integer id){
+    public Board boardView(Integer id) {
         return boardRepository.findById(id).get();
     }
 
     // 특정 게시글 삭제하기
-    public void boardDelete(Integer id){
+    public void boardDelete(Integer id) {
         boardRepository.deleteById(id);
     }
 }
