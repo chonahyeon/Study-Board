7cb0ae1d2f6f1a102643ced4a7d3c77d2cf7b062
nahyeon cho <whskgus12@nate.com>
2022-12-22 20:51:40 +0900

    feat: User 테이블과 Role 테이블 연결하기
    User 테이블과 Role 테이블을 @ManyToMany 와 @JoinTable 에노테이션을 통해 다대다 관계를 설정합니다.

--- src/main/java/com/study/board/controller/AccountController.java
+++ src/main/java/com/study/board/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.study.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,10 @@ public class AccountController {
     @GetMapping("/login")
     public String login(){
         return "account/login";
+    }
+
+    @PostMapping("/register")
+    public String register(){
+        return "account/register";
     }
 }
